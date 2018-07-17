755c0f92e9fbfc8fb1c47c931035c6a81b725d3f
Indra Putra <indrakoslab@gmail.com>
2018-07-17 16:06:50 +0700

    add js file for button onclick

---
+++ index.js
@@ -0,0 +1,7 @@
+const loginAlert = document.getElementById("button-login");
+
+const login = () => {
+    alert("Hello there, Thanks for login in");
+}
+
+loginAlert.addEventListener('click', login);
